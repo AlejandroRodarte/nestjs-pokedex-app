1dc255189eb5f0d9c82ec0bb6b9ef8a7937f26cb
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-21 15:53:51 -0600

    extra: set docker secrets on pokedex project

---
+++ pokedex/src/docker/set-secrets.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+
+const setSecrets = () => {
+  const mongodbUrl = fs
+    .readFileSync(process.env.MONGODB_URL_FILE_PATH!, 'utf-8')
+    .trim();
+  process.env.MONGODB_URL = mongodbUrl;
+};
+
+if (['production-docker', 'development-docker'].includes(process.env.NODE_ENV!))
+  setSecrets();
