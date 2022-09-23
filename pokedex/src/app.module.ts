bd4eff075356a889387dce217f710cf4e5a1109c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-22 21:12:44 -0600

    89. Crear módulo SEED

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -6,6 +6,7 @@ import { HealthModule } from './health/health.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DbHelpersModule } from './db-helpers/db-helpers.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { CommonModule } from './common/common.module';
     HealthModule,
     DbHelpersModule,
     CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
