9005bd50c0df95a5d1e090290daafc4730abc0d2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-22 21:44:15 -0600

    90. Realizar petición http desde Nest

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  populate() {
-    return `This action populates the database`;
+  private readonly LIMIT = 1;
+  private readonly axios: AxiosInstance = axios;
+
+  async populate() {
+    const res = await this.axios.get<PokeResponse>(
+      `https://pokeapi.co/api/v2/pokemon?limit=${this.LIMIT}`,
+    );
+    const pokemons = res.data.results.map((smallPokemon) => {
+      const name = smallPokemon.name;
+      const [, , , , , , no] = smallPokemon.url.split('/');
+      return { name, no: +no };
+    });
+    return res.data;
   }
 }
